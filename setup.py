0be2d4f007bdc54bd33441b6196a70b5a358d606
MVladislav <dev@mvladislav.online>
2022-01-05 17:07:51 +0100

    refactor: envsetup improve and test improve

--- setup.py
+++ setup.py
@@ -5,12 +5,47 @@
 import os
 from subprocess import check_call
 
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 
+try:
+    from dotenv import load_dotenv
+    load_dotenv('.env_project')
+except ImportError:
+    import logging
+    import sys
+
+    source_to_install = 'python-dotenv'
+    logging.log(logging.CRITICAL, f'Failed to Import {source_to_install}')
+    logging.log(logging.INFO, f'Attempting to Install {source_to_install}')
+    try:
+        import subprocess
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', '-q', source_to_install])
+        from dotenv import load_dotenv
+        load_dotenv('.env_project')
+        logging.log(logging.INFO, '[DONE]')
+    except Exception:
+        logging.log(logging.CRITICAL, '[FAIL]')
+
 PROJECT_NAME: str = os.getenv('PROJECT_NAME', 'vm_clockify')
 VERSION: str = os.getenv('VERSION', '0.0.2')
+SCRIPT_INSTALL: str = os.getenv('VM_SCRIPT_INSTALL', 'no')
+
+
+def main():
+    PROJECT_NAME_SLUG = slugify(PROJECT_NAME)
+    setup(
+        cmdclass={
+            'develop': PostDevelopCommand,
+            'install': PostInstallCommand,
+        },
+        install_requires=read_requirements(),
+        entry_points=f'''
+            [console_scripts]
+            {PROJECT_NAME_SLUG}=app.main:cli
+        ''',
+    )
 
 # ------------------------------------------------------------------------------
 #
@@ -25,7 +60,8 @@ class PostDevelopCommand(develop):
     '''
 
     def run(self):
-        check_call(['/bin/bash', './scripts/setup-dev.sh'])
+        if isinstance(SCRIPT_INSTALL, str) and SCRIPT_INSTALL == 'yes':
+            check_call(['/bin/bash', './scripts/setup-dev.sh'])
         develop.run(self)
 
 
@@ -35,7 +71,8 @@ class PostInstallCommand(install):
     '''
 
     def run(self):
-        check_call(['/bin/bash', './scripts/setup.sh'])
+        if isinstance(SCRIPT_INSTALL, str) and SCRIPT_INSTALL == 'yes':
+            check_call(['/bin/bash', './scripts/setup.sh'])
         install.run(self)
 
 # ------------------------------------------------------------------------------
@@ -89,29 +126,5 @@ def slugify(value, allow_unicode=False):
 # ------------------------------------------------------------------------------
 
 
-PROJECT_NAME_SLUG = slugify(PROJECT_NAME)
-
-setup(
-    name=PROJECT_NAME,
-    version=VERSION,
-    license='GNU AGPLv3',
-    description=PROJECT_NAME,
-    long_description=read_long_description(),
-    long_description_content_type='text/markdown',
-    author='MVladislav',
-    author_email='info@mvladislav.online',
-    packages=find_packages(),
-    data_files=[('', ['requirements.txt', 'scripts/setup.sh', 'scripts/setup-dev.sh'])],
-    include_package_data=True,
-    cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
-    },
-    install_requires=read_requirements(),
-    python_requires='>=3.8',
-    zip_safe=True,
-    entry_points=f'''
-        [console_scripts]
-        {PROJECT_NAME_SLUG}=app.main:cli
-    ''',
-)
+if __name__ == "__main__":
+    main()
